b32bd67dbb23c90560c4e0bea6080c75dfb0752c
NlGBOB <nlgbobmail@gmail.com>
2025-10-27 23:52:58 +0200

    shorten run func

---
+++ 2_scheduler_commented.js
@@ -0,0 +1,63 @@
+taskHeads = {};
+tasksByTag = {};
+currentTick = 0;
+
+run = (task, delay, tag) => {
+    const targetTick = currentTick + delay;
+    taskHeads[targetTick] = { task, next: taskHeads[targetTick], tag }
+    tasksByTag[tag] = { handle: schedulerNode, next: tasksByTag[tag] };
+};
+
+
+runWhile = (task, conditional, step, tag, onComplete) => {
+    const stepRunner = () => {
+        if (conditional()) {
+            task();
+            run(stepRunner, step, tag);
+        } else run(onComplete, 1, tag);
+    }
+    stepRunner()
+};
+
+
+repeat = (task, interval, tag) => {
+    const handle = { task };
+    const repeater = () => {
+        if (!handle.task) return;
+        handle.task();
+        run(repeater, interval, tag);
+    };
+    tasksByTag[tag] = { handle, next: tasksByTag[tag] }
+    repeater();
+};
+
+
+clearByTag = (tag) => {
+    for (
+        let currentTagNode = tasksByTag[tag];
+        currentTagNode;
+        currentTagNode = currentTagNode.next
+    ) currentTagNode.handle.task = () => { };
+    delete tasksByTag[tag];
+};
+
+sequence = (tasks, step, tag, onComplete) => {
+    let index = 0;
+    const runNextJob = () => {
+        tasks[index]();
+        if (index + 1 < tasks.length) {
+            index++;
+            run(runNextJob, step, tag)
+        } else run(onComplete, 1, tag)
+    }
+    runNextJob();
+};
+
+tick = () => {
+    while (taskHeads[currentTick]) {
+        const headNode = taskHeads[currentTick];
+        headNode.task();
+        taskHeads[currentTick] = headNode.next;
+    }
+    delete taskHeads[currentTick++];
+};
