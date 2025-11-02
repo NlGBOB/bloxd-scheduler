b9f51224ea7e3ddc53ff70916d568696a1086027
NlGBOB <nlgbobmail@gmail.com>
2025-11-02 20:44:04 +0200

    scheduler v2 update

--- scheduler_v2_temp.js
+++ scheduler_v2_temp.js
@@ -1,31 +1,28 @@
 S = {
     tasks: {},
+    tags: {},
     current: 0,
     processing: null,
     dummy: [],
     currentTickList: null,
     dispatcher: {
         get 1() {
-            S.currentTickList[0][0]();
-            S.currentTickList[0] = S.currentTickList[0][3];
+            const nodeToRun = S.currentTickList[0];
+            const tag = nodeToRun[1];
+            const tagList = S.tags[tag];
+            nodeToRun[0]();
+            const prevTag = nodeToRun[4];
+            const nextTag = nodeToRun[5];
+            [S.dummy, prevTag][+!!prevTag][5] = nextTag;
+            [S.dummy, nextTag][+!!nextTag][4] = prevTag;
+            const isTagHead = +(tagList[0] === nodeToRun);
+            tagList[0] = [tagList[0], nextTag][isTagHead];
+            const isTagTail = +(tagList[1] === nodeToRun);
+            tagList[1] = [tagList[1], prevTag][isTagTail];
+            const tagListIsEmpty = +!tagList[0];
+            ({ get 1() { delete S.tags[tag]; } })[tagListIsEmpty];
+            S.currentTickList[0] = nodeToRun[3];
             S.dispatcher[+!!S.currentTickList[0]];
-        }
-    },
-    clearer: {
-        tagsToClear: null,
-        tickList: null,
-        prev: null,
-        current: null,
-        get 1() {
-            const next = S.clearer.current[3];
-            const shouldRemove = +!!S.clearer.tagsToClear.includes(S.clearer.current[1]);
-            const isTail = +(S.clearer.tickList[1] === S.clearer.current);
-            const removingTheTail = isTail & shouldRemove;
-            S.clearer.prev[3] = [S.clearer.prev[3], next][shouldRemove];
-            S.clearer.prev = [S.clearer.current, S.clearer.prev][shouldRemove];
-            S.clearer.tickList[1] = [S.clearer.tickList[1], S.clearer.prev][removingTheTail];
-            S.clearer.current = next;
-            S.clearer[+!!S.clearer.current];
         }
     },
     run(task, delay, tag) {
@@ -36,12 +33,19 @@ S = {
         const isSameTick = +(targetTick === S.processing);
         const isSchedulingForCurrentExecutingTick = isProcessing & isSameTick;
         targetTick = targetTick + isSchedulingForCurrentExecutingTick;
+        const schedulerNode = [task, effectiveTag, null, null, null, null, targetTick];
         S.tasks[targetTick] = [[null, null], S.tasks[targetTick]][+!!S.tasks[targetTick]];
-        const oldTail = S.tasks[targetTick][1];
-        const schedulerNode = [task, effectiveTag, oldTail, null];
-        [S.dummy, oldTail][+!!oldTail][3] = schedulerNode;
+        const oldTickTail = S.tasks[targetTick][1];
+        schedulerNode[2] = oldTickTail;
+        [S.dummy, oldTickTail][+!!oldTickTail][3] = schedulerNode;
         S.tasks[targetTick][0] = [schedulerNode, S.tasks[targetTick][0]][+!!S.tasks[targetTick][0]];
         S.tasks[targetTick][1] = schedulerNode;
+        S.tags[effectiveTag] = [[null, null], S.tags[effectiveTag]][+!!S.tags[effectiveTag]];
+        const oldTagTail = S.tags[effectiveTag][1];
+        schedulerNode[4] = oldTagTail;
+        [S.dummy, oldTagTail][+!!oldTagTail][5] = schedulerNode;
+        S.tags[effectiveTag][0] = [schedulerNode, S.tags[effectiveTag][0]][+!!S.tags[effectiveTag][0]];
+        S.tags[effectiveTag][1] = schedulerNode;
     },
     while(task, conditional, step, tag, onComplete) {
         const effectiveConditional = [() => { return false }, conditional][+!!conditional];
@@ -66,20 +70,52 @@ S = {
         repeater();
     },
     cancel(tags) {
-        S.clearer.tagsToClear = [tags, [tags]][+!Array.isArray(tags)];
-        Object.keys(S.tasks).forEach(tickKey => {
-            const tickList = S.tasks[tickKey];
-            const dummy = [null, '__dummy', null, tickList[0]];
-            S.clearer.tickList = tickList;
-            S.clearer.prev = dummy;
-            S.clearer.current = tickList[0];
-            S.clearer[+!!S.clearer.current];
-            tickList[0] = dummy[3];
-            const tailIsDummy = +(tickList[1] === dummy);
-            tickList[1] = [tickList[1], null][tailIsDummy];
-            const listIsEmpty = +!tickList[0];
-            ({ get 1() { delete S.tasks[tickKey]; } })[listIsEmpty];
-        });
+        const remover = {
+            tagsToClear: [[], tags][+!tags],
+            tagIndex: 0,
+            currentNode: null,
+            currentTag: null,
+            get 1() {
+                const hasMoreTags = +(remover.tagIndex < remover.tagsToClear.length);
+                ({
+                    get 1() {
+                        remover.currentTag = remover.tagsToClear[remover.tagIndex];
+                        const tagListExists = +!!S.tags[remover.currentTag];
+                        remover.currentNode = [null, S.tags[remover.currentTag][0]][tagListExists];
+                        remover[2 + !tagListExists];
+                    }
+                })[hasMoreTags];
+            },
+            get 2() {
+                const nodeToCancel = remover.currentNode;
+                remover.currentNode = nodeToCancel[5];
+
+                const tick = nodeToCancel[6];
+                const tickList = S.tasks[tick];
+                const prevTick = nodeToCancel[2];
+                const nextTick = nodeToCancel[3];
+
+                [S.dummy, prevTick][+!!prevTick][3] = nextTick;
+                [S.dummy, nextTick][+!!nextTick][2] = prevTick;
+
+                const isTickHead = +(tickList[0] === nodeToCancel);
+                tickList[0] = [tickList[0], nextTick][isTickHead];
+                const isTickTail = +(tickList[1] === nodeToCancel);
+                tickList[1] = [tickList[1], prevTick][isTickTail];
+
+                const tickListIsEmpty = +!tickList[0];
+                ({ get 1() { delete S.tasks[tick]; } })[tickListIsEmpty];
+
+                const hasMoreNodes = +!!remover.currentNode;
+                remover[2 + !hasMoreNodes];
+            },
+            get 3() {
+                delete S.tags[remover.currentTag];
+                remover.tagIndex++;
+                remover[1];
+            },
+        };
+        remover[+!!(remover.tagsToClear.length > 0)];
     }
 };
 
